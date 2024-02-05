a4928023e0c965ddf76e04792eb43ea8088d2d48
dfyockey <git@diffengine.net>
2024-02-04 20:48:27 -0500

    Add ActionBar and content to About activity

--- app/src/main/java/net/diffengine/romandigitalclock/AboutActivity.java
+++ app/src/main/java/net/diffengine/romandigitalclock/AboutActivity.java
@@ -1,5 +1,6 @@
 package net.diffengine.romandigitalclock;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
