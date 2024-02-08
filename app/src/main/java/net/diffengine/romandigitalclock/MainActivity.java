bad71f6a3f7ac14ca24033484913492b025f2008
dfyockey <git@diffengine.net>
2024-02-08 12:37:13 -0500

    Add option to keep screen on only when charging

--- app/src/main/java/net/diffengine/romandigitalclock/MainActivity.java
+++ app/src/main/java/net/diffengine/romandigitalclock/MainActivity.java
@@ -9,7 +9,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -20,18 +24,37 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtTime;
     private View     bkgndView;
     private Handler  myHandler;
-    private boolean  ampm;
-    private boolean  ampmSeparator;
+
+    private boolean ampm;
+    private boolean ampmSeparator;
+    private boolean keepon;
+    private boolean onlywhencharging;
+
+    private final   Context context = this;
 
     private WindowInsetsControllerCompat windowInsetsControllerCompat;
 
     private Fragment menuFragment;
 
+    /** @noinspection ReassignedVariable*/
+    private void setKeepScreenOn() {
+        boolean keepScreenOn = false;
+        if (keepon) {
+            if (!onlywhencharging || isCharging()) {
+                keepScreenOn = true;
+            }
+        }
+        bkgndView.setKeepScreenOn(keepScreenOn);
+    }
+
     private final Runnable updatetime = new Runnable() {
         @Override
         public void run() {
-            int updatedelay_in_ms = 1000;
+            int updatedelay_in_ms = 200;    // Must be less than the minimum screen timeout (15sec)
+
             txtTime.setText(romantime.now(ampm, ampmSeparator));
+            setKeepScreenOn();
+
             myHandler.postDelayed(updatetime, updatedelay_in_ms);
         }
     };
@@ -49,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
     }
 
+    /** @noinspection Convert2Lambda*/
     View.OnClickListener bkgndOCL = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -56,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    /** @noinspection Convert2Lambda*/
     View.OnClickListener clockOCL = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -73,14 +98,38 @@ public class MainActivity extends AppCompatActivity {
                 case "chkbox_ampm_separator":
                     ampmSeparator = sp.getBoolean(key, false);
                     break;
-                case "chkbox_always_on":
-                    bkgndView.setKeepScreenOn(sp.getBoolean(key, false));
+                case "chkbox_keep_on":
+                    keepon = sp.getBoolean(key, false);
+                    setKeepScreenOn();
+                    //bkgndView.setKeepScreenOn(keepon);
+                    break;
+                case "chkbox_when_charging":
+                    onlywhencharging = sp.getBoolean(key, false);
                     break;
                 default:
                     break;
             }
         }
     };
+    
+    private boolean isCharging() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, ifilter);
+
+        // Are we charging / charged?
+        if (batteryStatus != null) {
+            int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            return (
+                    status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    status == BatteryManager.BATTERY_STATUS_FULL
+            );
+        } else {
+            // Returning `true` in this case should prevent the display from coming on
+            // if it's already off, with the correct charging state being returned
+            // on the next non-null batteryStatus.
+            return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,7 +163,9 @@ public class MainActivity extends AppCompatActivity {
         sp.registerOnSharedPreferenceChangeListener(prefChgListener);
         ampm          = sp.getBoolean("chkbox_format",        false);
         ampmSeparator = sp.getBoolean("chkbox_ampm_separator",false);
-        bkgndView.setKeepScreenOn(sp.getBoolean("chkbox_always_on",false));
+        keepon        = sp.getBoolean("chkbox_keep_on",       false);
+        onlywhencharging = sp.getBoolean("chkbox_when_charging", false);
+        // No need to set Keep Screen On state here; it will be set each time updatetime is called.
     }
 
     protected void onPause() {
