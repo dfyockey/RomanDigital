5bf810564ccd41323babfad5ca3ce0fb424511f5
dfyockey <git@diffengine.net>
2024-03-11 17:25:18 -0400

    Change to update at 15s freq y on option changes; remove redundant code

--- app/src/main/java/net/diffengine/romandigitalclock/TimeDisplayWidget.java
+++ app/src/main/java/net/diffengine/romandigitalclock/TimeDisplayWidget.java
@@ -7,56 +7,46 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
-/**
- * Implementation of App Widget functionality.
- */
 public class TimeDisplayWidget extends AppWidgetProvider {
 
     static AlarmManager alarmManager;
     static PendingIntent alarmIntent;
     public static final String MINUTE_TICK = "net.diffengine.romandigitalclock.MINUTE_TICK";
-    final int ONE_MINUTE = 60000;
+    final int FIFTEEN_SECS = 15000;
+
+    private static RemoteViews updateTimeDisplay(Context context) {
+        CharSequence widgetText = romantime.now(true, true, false);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.time_display_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+        return views;
+    }
+
+    private void onTick (Context context, AppWidgetManager appWidgetManager) {
+        ComponentName widgetName = new ComponentName(context.getPackageName(), TimeDisplayWidget.class.getName());
+        appWidgetManager.updateAppWidget(widgetName, updateTimeDisplay(context));
+    }
+
+    private void onTick (Context context) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        onTick(context, appWidgetManager);
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
         String action = intent.getAction();
         if (action.equals(MINUTE_TICK)) {
-            CharSequence widgetText = romantime.now(true, true, false);
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.time_display_widget);
-            views.setTextViewText(R.id.appwidget_text, widgetText);
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            ComponentName widgetName = new ComponentName(context.getPackageName(), TimeDisplayWidget.class.getName());
-            appWidgetManager.updateAppWidget(widgetName, views);
-            //int[] appWidgetIds = appWidgetManager.getAppWidgetIds(widgetName));
-
-//            for (int appWidgetId : appWidgetIds) {
-//
-//                CharSequence widgetText = romantime.now(true, true, false);
-//                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.time_display_widget);
-//                views.setTextViewText(R.id.appwidget_text, widgetText);
-//
-//                appWidgetManager.updateAppWidget(appWidgetId, views);
-//            }
-
-            //this.onUpdate(context, appWidgetManager, appWidgetIds);
-        } else {
-            super.onReceive(context, intent);
+            onTick(context);
         }
     }
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        CharSequence widgetText = romantime.now(true, true, false);
-
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.time_display_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(appWidgetId, updateTimeDisplay(context));
     }
 
     @Override
@@ -68,6 +58,11 @@ public class TimeDisplayWidget extends AppWidgetProvider {
     }
 
     @Override
+    public void onAppWidgetOptionsChanged (Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        onTick(context, appWidgetManager);
+    }
+
+    @Override
     public void onEnabled(Context context) {
         if (alarmManager == null) {
             alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -76,7 +71,7 @@ public class TimeDisplayWidget extends AppWidgetProvider {
                 alarmIntent = PendingIntent.getBroadcast(
                         context, 0, tickIntent, PendingIntent.FLAG_IMMUTABLE
                 );
-            alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), ONE_MINUTE, alarmIntent);
+            alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), FIFTEEN_SECS, alarmIntent);
         }
     }
 
