aadc0b0a6762c84d811c3857a6e3698a718f1934
dfyockey <git@diffengine.net>
2026-01-12 12:24:50 -0500

    chore: add or update license notices

--- app/src/main/java/net/diffengine/romandigitalclock/TimeDisplayWidget.java
+++ app/src/main/java/net/diffengine/romandigitalclock/TimeDisplayWidget.java
@@ -2,7 +2,7 @@
  * TimeDisplayWidget.java
  * - This file is part of the Android app RomanDigital
  *
- * Copyright 2024-2025 David Yockey
+ * Copyright 2024-2026 David Yockey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
