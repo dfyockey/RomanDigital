3fc20d4e8c079a090905f404b346f3ef550f0b54
dfyockey <git@diffengine.net>
2026-01-20 13:13:59 -0500

    cleanup: remove redundant service launch
    - Remove launch of the time tick relay service on click of the Save
      button, the launch having been rendered redundant by the addition of
      startRelayIfWidgets calls in the immediately preceding commit
      (c8b35969).

--- app/src/main/java/net/diffengine/romandigitalclock/SettingsButtonBarFragment.java
+++ app/src/main/java/net/diffengine/romandigitalclock/SettingsButtonBarFragment.java
@@ -25,10 +25,8 @@ import static android.app.Activity.RESULT_OK;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -112,16 +110,6 @@ public class SettingsButtonBarFragment extends Fragment implements View.OnClickL
                 // See https://stackoverflow.com/a/40709721
                 Intent result = new Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
                 parentActivity.setResult(RESULT_OK, result);
-
-                // Since we're here because appWidgetId is valid, we're in a widget config activity,
-                // so make sure the tick relay service is started.
-                Context context = parentActivity;
-                Intent serviceIntent = new Intent(context, TimeTickRelay.class);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    context.startForegroundService(serviceIntent);
-                } else {
-                    context.startService(serviceIntent);
-                }
             }
         } else {
             // In case of some shortsighted modification... :)
