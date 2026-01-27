689b44255114cf9917ae4d34f849bdcf091656c4
dfyockey <git@diffengine.net>
2026-01-27 13:58:46 -0500

    build: upgrade gradle plugin, java, dependencies
    Do following, except for the dependencies, primarily to try to eliminate
    the warning pertaining to android.permission.FOREGROUND_SERVICE in the
    manifest.
    
    - Upgrade the gradle plugin to version 9.0.0
    
    - Change android.newDsl in gradle.properties to true so as to avoid
      using the deprecated function `org.gradle.api.Project.android` in the
      app's gradle build file.
    
    - Upgrade source and target compatibility from Java 8 to Java 21
    
    - Upgrade dependencies just to bring them up to date

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,13 @@ plugins {
 
 android {
     namespace = "net.diffengine.romandigitalclock"
+    //noinspection GradleDependency
     compileSdk = 35
 
     defaultConfig {
         applicationId = "net.diffengine.romandigitalclock"
         minSdk = 21
+        //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 15
         versionName = "3.0.1 Beta"
@@ -23,8 +25,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         viewBinding = true
@@ -36,12 +38,12 @@ dependencies {
     val acraVersion = "5.13.1"
 
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.preference:preference:1.2.1")
     implementation("ch.acra:acra-mail:$acraVersion")
     implementation("ch.acra:acra-dialog:$acraVersion")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
