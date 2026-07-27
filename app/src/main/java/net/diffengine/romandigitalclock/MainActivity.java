dfyockey/RomanDigital

commit 71728bd4fb92c7870cc13c8fce3332bb4da38376

perf(app): reduce reset of unchanged settings
- Add a separate function to update only the time rather than the entire
  app display.

- Add a separate custom BroadcastReceiver to call the added function on
  receipt of an ACTION_TIME_TICK intent rather than calling the function
  to update the entire app display.

app/src/main/java/net/diffengine/romandigitalclock/MainActivity.java
@@ -135,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
 
     int text_resize_attempt_count = 0;
 
-    private void updateTimeDisplay() {
+    private void updateDisplay() {
         // Negate romantime.now arguments where needed to accommodate chosen state arrangement of
         // a/b switches, where false/true states depend on chosen left/right positions
         String now = romantime.now( !getPref(ampm), getPref(ampmSeparator), !getPref(alignment), TimeZone.getDefault().getID() );
@@ -169,17 +169,31 @@ public class MainActivity extends AppCompatActivity {
         setKeepScreenOn();
     }
 
+    private void updateTimeDisplay() {
+        // Negate romantime.now arguments where needed to accommodate chosen state arrangement of
+        // a/b switches, where false/true states depend on chosen left/right positions
+        String now = romantime.now( !getPref(ampm), getPref(ampmSeparator), !getPref(alignment), TimeZone.getDefault().getID() );
+        TimeDisplay.setText(now);
+    }
+
     //---------------------------------------------------------------
 
-    private class BroadcastReceiverEx extends BroadcastReceiver {
+    private class TimeTickBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             updateTimeDisplay();
         }
     }
 
+    private class BroadcastReceiverEx extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateDisplay();
+        }
+    }
+
     // Receiver instance to be registered as exported for receiving system-broadcast ACTION_TIME_TICK intent
-    private final BroadcastReceiverEx broadcastReceiver = new BroadcastReceiverEx();
+    private final TimeTickBroadcastReceiver timeTickBroadcastReceiver = new TimeTickBroadcastReceiver();
 
     // Receiver instance to be registered as RECEIVER_NOT_EXPORTED for receiving app-broadcast UPDATE_DISPLAY intent
     private final BroadcastReceiverEx updateReceiver = new BroadcastReceiverEx();
@@ -390,7 +404,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onPause() {
         unregisterReceiver(updateReceiver);
-        unregisterReceiver(broadcastReceiver);
+        unregisterReceiver(timeTickBroadcastReceiver);
         findViewById(R.id.my_toolbar).setVisibility(View.INVISIBLE);
 
         // Broadcast an intent immediately after either Close or Save is pressed
@@ -422,7 +436,7 @@ public class MainActivity extends AppCompatActivity {
         setDisplayColorFromPref();
         setDisplayFont("roboto");
 
-        registerReceiver(broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+        registerReceiver(timeTickBroadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
         ContextCompat.registerReceiver(context, updateReceiver, new IntentFilter(UPDATE_DISPLAY), ContextCompat.RECEIVER_NOT_EXPORTED);
         text_resize_attempt_count = 0;
         sendBroadcast(makeIntent(UPDATE_DISPLAY));

commit d0f2e7234b6820070e903c2231c35a14e965cc59

fix(app): vertical clock layout merge regression
Addition to MainActivity of a new updateTimeDisplay() method and
renaming of original updateTimeDisplay() method to updateDisplay() in
commit 71728bd4 caused the time display to revert to horizontal layout
even when set for vertical layout. This occurred after merge of 71728bd4
from reduce-app-resource-usage branch into commit 279e1f0b from
vertical-clock-layout branch. Lacking any code for getting clock display
text for the vertical layout, the new updateTimeDisplay() method added
by the merge only returns the horizontal layout text. The vertical
layout text was formerly provided by the original updateTimeDisplay()
method, which is now only used for full update of the display.

- Fix the regression by modifying the new updateTimeDisplay() method to
  get the clock display text in horizontal or vertical layout form
  according to the app's layout preference setting.

app/src/main/java/net/diffengine/romandigitalclock/MainActivity.java
@@ -139,14 +139,7 @@ public class MainActivity extends AppCompatActivity {
     float displayLineSpacing = 0;
 
     private void updateDisplay() {
-        // Negate romantime.now arguments where needed to accommodate chosen state arrangement of
-        // a/b switches, where false/true states depend on chosen left/right positions
-        String now;
-        if (getPref("switch_layout") == left) {
-            now = romantime.now(!getPref(ampm), getPref(ampmSeparator), !getPref(alignment), TimeZone.getDefault().getID());
-        } else {
-            now = romantime.now(!getPref(ampm), TimeZone.getDefault().getID());
-        }
+        String now = getCurrentRomanTime();
 
         // IMPORTANT:
         // For the String returned by romantime.now to be correctly aligned in TimeDisplay textview,
@@ -196,10 +189,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateTimeDisplay() {
+        TimeDisplay.setText(getCurrentRomanTime());
+    }
+
+    private String getCurrentRomanTime() {
         // Negate romantime.now arguments where needed to accommodate chosen state arrangement of
         // a/b switches, where false/true states depend on chosen left/right positions
-        String now = romantime.now( !getPref(ampm), getPref(ampmSeparator), !getPref(alignment), TimeZone.getDefault().getID() );
-        TimeDisplay.setText(now);
+        String now;
+        if (getPref("switch_layout") == left) {
+            now = romantime.now(!getPref(ampm), getPref(ampmSeparator), !getPref(alignment), TimeZone.getDefault().getID());
+        } else {
+            now = romantime.now(!getPref(ampm), TimeZone.getDefault().getID());
+        }
+        return now;
     }
 
     //---------------------------------------------------------------