60d82ad85097ec7c172b092af27d6123be6da937
dfyockey <git@diffengine.net>
2026-08-13 16:23:57 -0400

    build: bump up versionName and versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 21
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 17
-        versionName = "3.0.2"
+        versionCode = 18
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
